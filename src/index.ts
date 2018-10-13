971b3a29c3c11678920e9246676f8ed496798f81
gauravmahto <gauravm.git@gmail.com>
2018-10-13 10:43:55 +0530

    Inital commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Copyright 2018 - Author gauravm.git@gmail.com
+ */
+
+const logger = console;
+
+logger.info('Program started.');
---
+++ typings.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2018 - Author gauravm.git@gmail.com
+ */
+
+declare module NodeJS {
+
+  interface Global {
+
+  }
+
+}
