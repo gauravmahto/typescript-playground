2bcf0adac6f30917973015e8b1a99b662554d6e3
gauravmahto <gauravm.git@gmail.com>
2018-10-13 12:38:38 +0530

    Added TS v3.1 tests

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,23 @@
 const logger = console;
 
 logger.info('Program started.');
+
+// #region Mappable tuple and array types.
+
+// Examples taken from:
+// https://blogs.msdn.microsoft.com/typescript/announcing-typescript-3-1/#mappable-tuple-and-array-types
+
+type Stringify<T> = {
+  [K in keyof T]: string
+};
+
+function stringifyAll<T extends Array<unknown>>(...elements: T): Stringify<T> {
+
+  return elements.map((x) => String(x)) as Stringify<T>;
+
+}
+
+const out = stringifyAll(1, 'a', true, {});
+logger.log(out.length); // Length is preserved as not everything is stringified as [K in ..]: string
+
+// #endregion Mappable tuple and array types.
